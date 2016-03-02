46abc822825073aa784e42a9e935b76dc7426add
gujiangfei <gujiangfeimail@163.com>
2016-03-02 20:36:28 +0800

    1

--- calculatedata.py
+++ calculatedata.py
@@ -6,23 +6,26 @@ rootdir = 'data/'
 
 for parent,dirnames,filenames in os.walk(rootdir):
     for filename in filenames:
+        namelist = []
+        nolist = []
         maxlist = []
         minlist = []
         pricelist = []
         f = open('data/'+filename,'r+')
         for i in f.readlines():
             tmp = i.split('#')
-            maxlist.append(tmp[0])
-            minlist.append(tmp[1])
-            pricelist.append(tmp[2])
+            namelist.append(tmp[0])
+            nolist.append(tmp[1])
+            maxlist.append(tmp[2])
+            minlist.append(tmp[3])
+            pricelist.append(tmp[4])
         f.close()
 
         #排序
-        maxlist.sort()
+        maxlist.sort(reverse= True)
         minlist.sort()
-        pricelist.sort()
 
         f2 = open('result/'+filename,'w')
-        f2.write(maxlist[0]+'#'+minlist[0]+'#'+pricelist[0])
+        f2.write(namelist[0]+'#'+nolist[0]+'#'+maxlist[0]+'#'+minlist[0]+'#'+pricelist[-1])
         f2.close()
         # print(maxlist,minlist,pricelist)
