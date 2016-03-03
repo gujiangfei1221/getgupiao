8190064ac4ae933fe14e0a68920f5e877706264a
gujiangfei1221 <929008776@qq.com>
2016-03-03 15:36:41 +0800

    1

---
+++ calculatedata2.py
@@ -0,0 +1,26 @@
+#-*- coding=utf-8 -*-
+import os.path
+
+rootdir = 'data2/'
+
+
+for parent,dirnames,filenames in os.walk(rootdir):
+    for filename in filenames:
+        namelist = []
+        nolist = []
+        minlist = []
+        f = open('data/'+filename,'r+')
+        for i in f.readlines():
+            tmp = i.split('#')
+            namelist.append(tmp[0])
+            nolist.append(tmp[1])
+            minlist.append(tmp[2])
+        f.close()
+
+        #排序
+        minlist.sort()
+
+        f2 = open('result2/'+filename,'w')
+        f2.write(namelist[0]+'#'+nolist[0]+'#'+minlist[0])
+        f2.close()
+        # print(maxlist,minlist,pricelist)
