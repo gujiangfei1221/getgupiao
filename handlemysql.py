f919bf5554b5a68d8359cd54061a2b77f3cfe07c
gujiangfei1221 <929008776@qq.com>
2016-03-09 11:19:32 +0800

    1

--- handlemysql.py
+++ handlemysql.py
@@ -27,7 +27,7 @@ for parent,dirnames,filenames in os.walk(rootdir):
             conn=pymysql.connect(host='localhost',user='root',passwd='root',db='codeigniter',port=3306,charset='utf8')
             cur=conn.cursor()#获取一个游标
             # cur.execute("update gupiaoinfo set latestlowprice = list[3],latesthighprice = list[2],price = list[4],longlouwprice = list[5] where gupiaoname = list[0]")
-            cur.execute("insert into gupiaoinfo(gupiaoname,latestlowprice,latesthighprice,longlowprice,price) values(\'"+list2[0]+'\',\''+list2[3]+'\',\''+list2[2]+'\',\''+list2[5]+'\',\''+list2[4]+'\')')
+            cur.execute('insert into gupiaoinfo(gupiaoname,latestlowprice,latesthighprice,longlowprice,price) values(\''+list2[0]+'\',\''+list2[3]+'\',\''+list2[2]+'\',\''+list2[5]+'\',\''+list2[4]+'\')')
             conn.commit();
             cur.close()#关闭游标
             conn.close()#释放数据库资源
