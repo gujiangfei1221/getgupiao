32f4dce085c91f329197a5e0a2167061c3fb3b6c
gujiangfei1221 <gujiangfei1221@163.com>
2016-03-24 21:47:41 +0800

    1

--- handledata2.py
+++ handledata2.py
@@ -2,7 +2,7 @@
 
 import pymysql,time,os,sys
 
-rootdir = 'data/'
+rootdir = '/root/gupiao/data/'
 
 def datetrans(tdate):
         spdate = tdate.replace("/","-")
@@ -19,7 +19,7 @@ def daysdiff(d1,d2):
 
 for parent,dirnames,filenames in os.walk(rootdir):
     for filename in filenames:
-        f = open('data/'+filename,'r+')
+        f = open('/root/gupiao/data/'+filename,'r+')
         for i in f.readlines():
             tmp = i.split('#')
             gupiaono = tmp[1]
@@ -29,7 +29,7 @@ for parent,dirnames,filenames in os.walk(rootdir):
             #获取一个数据库连接，注意如果是UTF-8类型的，需要制定数据库
             conn=pymysql.connect(host='localhost',user='root',passwd='root',db='codeigniter',port=8889,charset='utf8')
             cur=conn.cursor()#获取一个游标
-            cur.execute('select yichangdate from gupiaoinfo where gupiaono = \'' + 'sh600061' +'\' and yichang = \'' + 'yichang' +'\'')
+            cur.execute('select yichangdate from gupiaoinfo where gupiaono = \'' + gupiaono +'\' and yichang = \'' + 'yichang' +'\'')
             r = cur.fetchall()
             if(len(r) > 0):
                 date1 = time.strftime("%Y-%m-%d", time.localtime())
@@ -37,7 +37,7 @@ for parent,dirnames,filenames in os.walk(rootdir):
                 date1sec = datetrans(date1)
                 date2sec = datetrans(date2)
                 if(daysdiff(date1sec,date2sec) >= 3):
-                    cur.execute('update gupiaoinfo set yichang = \'zhengchang\', yichangdate = \''+ time.strftime("%Y-%m-%d", time.localtime()) +'\' where gupiaono = \''+ 'sh600061' +'\'')
+                    cur.execute('update gupiaoinfo set yichang = \'zhengchang\', yichangdate = \''+ time.strftime("%Y-%m-%d", time.localtime()) +'\' where gupiaono = \''+ gupiaono +'\'')
                 conn.commit();
             cur.close()#关闭游标
             conn.close()#释放数据库资源
